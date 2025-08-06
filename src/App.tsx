61873d8532e257b2ede7869890aed26af5a5dbee
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-06 09:05:17 +0530

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import "./App.css";
-
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

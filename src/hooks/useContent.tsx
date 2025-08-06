b17caa40636d8b1e086908d03936c9fe48de7c70
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-06 09:04:13 +0530

    feat: Add useContext hook and Deashboard page.

---
+++ src/hooks/useContent.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+
+export function useContent() {
+  const [contents, setContents] = useState([]);
+
+  function refresh() {
+    axios
+      .get<any>(`${BACKEND_URL}/api/v1/content`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setContents(response.data.content);
+      });
+  }
+
+  useEffect(() => {
+    refresh();
+    const interval = setInterval(() => {
+      refresh();
+    }, 10 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return { contents, refresh };
+}

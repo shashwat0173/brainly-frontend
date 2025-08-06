2319552535ca7f9059a58e11c842caa035b883f6
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-06 06:11:32 +0530

    feat: Final button component and icons for button

---
+++ src/components/Button.tsx
@@ -0,0 +1,45 @@
+import type { ReactElement } from "react";
+
+interface ButtonProps {
+  variant: "primary" | "secondary";
+  text: string;
+  startIcon: ReactElement;
+  onClick?: () => void;
+  fullWidth?: boolean;
+  loading?: boolean;
+}
+
+const variantClasses = {
+  primary: "bg-[#7164c0] text-white",
+  secondary: "bg-[#d9ddee] text-[#7164c0]",
+};
+
+const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center";
+
+export function Button({
+  variant,
+  text,
+  startIcon,
+  onClick,
+  fullWidth,
+  loading,
+}: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={
+        variantClasses[variant] +
+        " " +
+        defaultStyles +
+        `${fullWidth ? " w-full flex justify-center items-center" : ""} ${
+          loading ? "opacity-45	" : ""
+        }`
+      }
+      disabled={loading}
+    >
+      <div className="pr-2">{startIcon}</div>
+      {text}
+    </button>
+  );
+}
+
--- src/icons/PlusIcon.tsx
+++ src/icons/PlusIcon.tsx
@@ -1,20 +1,18 @@
-import { iconSizeVariants, type IconProps } from ".";
-
-export const PlusIcon = (props: IconProps) => {
+export function PlusIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
+      stroke-width="1.5"
       stroke="currentColor"
-      className={iconSizeVariants[props.size]}
+      className="size-5"
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        stroke-linecap="round"
+        stroke-linejoin="round"
         d="M12 4.5v15m7.5-7.5h-15"
       />
     </svg>
   );
-};
+}

e7d76c2379200e36b0aea905367b1eae7640fe5b
Shashwat Tripathi <shashwattripathi0173@gmail.com>
2025-08-06 07:07:36 +0530

    feat: final Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 export function Card({ title, link, type }: CardProps) {
   return (
     <div>
-      <div className="p-4 bg-white rounded-md border-gray-200 max-w-72 border min-h-48 min-w-72">
+      <div className="p-4 bg-white rounded-md border-gray-200 max-w-72  border min-h-48 min-w-72">
         <div className="flex justify-between">
           <div className="flex items-center text-md">
             <div className="text-gray-500 pr-2">
